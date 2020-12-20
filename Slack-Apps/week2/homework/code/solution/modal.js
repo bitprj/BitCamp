bab0669c21bec5a88666720c700dbca12c863819
Beau Hayes-Pollard <62896513+Bahburs@users.noreply.github.com>
2020-12-19 19:19:07 -0500

    Update modal.js

--- Slack-Apps/week2/homework/code/solution/modal.js
+++ Slack-Apps/week2/homework/code/solution/modal.js
@@ -1,3 +1,12 @@
+const { App } = require('@slack/bolt');
+const { config } = require('dotenv');
+
+config();
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+});
+
 // Listen for a slash command invocation
 app.command('/ticket', async ({ ack, body, client }) => {
   // Acknowledge the command request
@@ -59,3 +68,8 @@ app.command('/ticket', async ({ ack, body, client }) => {
     console.error(error);
   }
 });
+
+  (async () => {
+    await app.start(process.env.PORT || 3000);
+    console.log('⚡️ Bolt app is running! ⚡️');
+  })();
