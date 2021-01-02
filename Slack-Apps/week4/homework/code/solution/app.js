8c7456f0803ead927544ed6ba9f66f58440daa73
Fifi Teklemedhin <43687874+FifiTeklemedhin@users.noreply.github.com>
2021-01-02 11:58:16 -0700

    Update app.js

--- Slack-Apps/week4/homework/code/solution/app.js
+++ Slack-Apps/week4/homework/code/solution/app.js
@@ -1,8 +1,7 @@
-<<<<<<< HEAD
+
 const { App, ExpressReceiver } = require('@slack/bolt');
 const express = require('express');
-=======
->>>>>>> 9a198601099836cb94db078100afe44ccd0d14cc
+
 const axios = require('axios');
 
 const { config } = require('dotenv');
