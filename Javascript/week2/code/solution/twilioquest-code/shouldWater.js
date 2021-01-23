c4fbd95558b6e9a2423b6ff3912b6fead1b9ec5a
Danielle Anzelone <danzelo1@binghamton.edu>
2021-01-22 23:23:15 -0500

    deleting unecessary files

--- Javascript/week2/code/solution/twilioquest-code/shouldWater.js
+++
@@ -1,7 +0,0 @@
-const lifeStatus = process.argv[2];
-const dryLevel = process.argv[3];
-const dryNumber = Number(dryLevel);
-
-if (lifeStatus === '0' && dryNumber > 10) {
-    console.log("WATER");
-}
--- Javascript/week2/code/solution/twilioquest-code/treeLifeDetector.js
+++
@@ -1,7 +0,0 @@
-const treeStatus = process.argv[2];
-
-if (treeStatus === '0') {
-    console.log("alive");
-} else {
-    console.log("other");
-}
